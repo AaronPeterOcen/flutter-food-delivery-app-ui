2b17a9d56b690b057e3d496403b1dec919104322
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2025-12-10 14:49:18 +0300

    updating FoodOrderPage

--- lib/pages/FoodOrderPage.dart
+++ lib/pages/FoodOrderPage.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
 
 class FoodOrderPage extends StatefulWidget {
+  const FoodOrderPage({Key? key}) : super(key: key);
+
   @override
+  // ignore: library_private_types_in_public_api
   _FoodOrderPageState createState() => _FoodOrderPageState();
 }
 
@@ -13,16 +17,16 @@ class _FoodOrderPageState extends State<FoodOrderPage> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          backgroundColor: Color(0xFFFAFAFA),
+          backgroundColor: const Color(0xFFFAFAFA),
           elevation: 0,
           leading: IconButton(
-            icon: Icon(
+            icon: const Icon(
               Icons.arrow_back_ios,
               color: Color(0xFF3a3737),
             ),
             onPressed: () => Navigator.of(context).pop(),
           ),
-          title: Center(
+          title: const Center(
             child: Text(
               "Item Carts",
               style: TextStyle(
@@ -32,20 +36,20 @@ class _FoodOrderPageState extends State<FoodOrderPage> {
               textAlign: TextAlign.center,
             ),
           ),
-          brightness: Brightness.light,
           actions: <Widget>[
             CartIconWithBadge(),
           ],
+          systemOverlayStyle: SystemUiOverlayStyle.dark,
         ),
         body: SingleChildScrollView(
           child: Container(
-            padding: EdgeInsets.all(15),
+            padding: const EdgeInsets.all(15),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
                 Container(
-                  padding: EdgeInsets.only(left: 5),
-                  child: Text(
+                  padding: const EdgeInsets.only(left: 5),
+                  child: const Text(
                     "Your Food Cart",
                     style: TextStyle(
                         fontSize: 20,
@@ -54,7 +58,7 @@ class _FoodOrderPageState extends State<FoodOrderPage> {
                     textAlign: TextAlign.left,
                   ),
                 ),
-                SizedBox(
+                const SizedBox(
                   height: 10,
                 ),
                 CartItem(
@@ -62,7 +66,7 @@ class _FoodOrderPageState extends State<FoodOrderPage> {
                     productPrice: "\$96.00",
                     productImage: "ic_popular_food_1",
                     productCartQuantity: "2"),
-                SizedBox(
+                const SizedBox(
                   height: 10,
                 ),
                 CartItem(
@@ -70,20 +74,20 @@ class _FoodOrderPageState extends State<FoodOrderPage> {
                     productPrice: "\$65.08",
                     productImage: "ic_popular_food_4",
                     productCartQuantity: "5"),
-                SizedBox(
+                const SizedBox(
                   height: 10,
                 ),
-                PromoCodeWidget(),
-                SizedBox(
+                const PromoCodeWidget(),
+                const SizedBox(
                   height: 10,
                 ),
-                TotalCalculationWidget(),
-                SizedBox(
+                const TotalCalculationWidget(),
+                const SizedBox(
                   height: 10,
                 ),
                 Container(
-                  padding: EdgeInsets.only(left: 5),
-                  child: Text(
+                  padding: const EdgeInsets.only(left: 5),
+                  child: const Text(
                     "Payment Method",
                     style: TextStyle(
                         fontSize: 20,
@@ -92,10 +96,10 @@ class _FoodOrderPageState extends State<FoodOrderPage> {
                     textAlign: TextAlign.left,
                   ),
                 ),
-                SizedBox(
+                const SizedBox(
                   height: 10,
                 ),
-                PaymentMethodWidget(),
+                const PaymentMethodWidget(),
               ],
             ),
           ),
@@ -104,15 +108,17 @@ class _FoodOrderPageState extends State<FoodOrderPage> {
 }
 
 class PaymentMethodWidget extends StatelessWidget {
+  const PaymentMethodWidget({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Container(
       alignment: Alignment.center,
       width: double.infinity,
       height: 60,
-      decoration: BoxDecoration(boxShadow: [
+      decoration: const BoxDecoration(boxShadow: [
         BoxShadow(
-          color: Color(0xFFfae3e2).withOpacity(0.1),
+          color: Color(0xFFfae3e2),
           spreadRadius: 1,
           blurRadius: 1,
           offset: Offset(0, 1),
@@ -121,14 +127,15 @@ class PaymentMethodWidget extends StatelessWidget {
       child: Card(
         color: Colors.white,
         elevation: 0,
-        shape: RoundedRectangleBorder(
-          borderRadius: const BorderRadius.all(
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(
             Radius.circular(5.0),
           ),
         ),
         child: Container(
           alignment: Alignment.center,
-          padding: EdgeInsets.only(left: 10, right: 30, top: 10, bottom: 10),
+          padding:
+              const EdgeInsets.only(left: 10, right: 30, top: 10, bottom: 10),
           child: Row(
             children: <Widget>[
               Container(
@@ -139,7 +146,7 @@ class PaymentMethodWidget extends StatelessWidget {
                   height: 50,
                 ),
               ),
-              Text(
+              const Text(
                 "Credit/Debit Card",
                 style: TextStyle(
                     fontSize: 16,
@@ -156,15 +163,17 @@ class PaymentMethodWidget extends StatelessWidget {
 }
 
 class TotalCalculationWidget extends StatelessWidget {
+  const TotalCalculationWidget({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Container(
       alignment: Alignment.center,
       width: double.infinity,
       height: 150,
-      decoration: BoxDecoration(boxShadow: [
+      decoration: const BoxDecoration(boxShadow: [
         BoxShadow(
-          color: Color(0xFFfae3e2).withOpacity(0.1),
+          color: Color(0xFFfae3e2),
           spreadRadius: 1,
           blurRadius: 1,
           offset: Offset(0, 1),
@@ -173,15 +182,16 @@ class TotalCalculationWidget extends StatelessWidget {
       child: Card(
         color: Colors.white,
         elevation: 0,
-        shape: RoundedRectangleBorder(
-          borderRadius: const BorderRadius.all(
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(
             Radius.circular(5.0),
           ),
         ),
         child: Container(
           alignment: Alignment.center,
-          padding: EdgeInsets.only(left: 25, right: 30, top: 10, bottom: 10),
-          child: Column(
+          padding:
+              const EdgeInsets.only(left: 25, right: 30, top: 10, bottom: 10),
+          child: const Column(
             children: <Widget>[
               SizedBox(
                 height: 15,
@@ -264,14 +274,16 @@ class TotalCalculationWidget extends StatelessWidget {
 }
 
 class PromoCodeWidget extends StatelessWidget {
+  const PromoCodeWidget({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Container(
-        padding: EdgeInsets.only(left: 3, right: 3),
-        decoration: BoxDecoration(boxShadow: [
+        padding: const EdgeInsets.only(left: 3, right: 3),
+        decoration: const BoxDecoration(boxShadow: [
           BoxShadow(
-            color: Color(0xFFfae3e2).withOpacity(0.1),
+            color: Color(0xFFfae3e2),
             spreadRadius: 1,
             blurRadius: 1,
             offset: Offset(0, 1),
@@ -279,17 +291,18 @@ class PromoCodeWidget extends StatelessWidget {
         ]),
         child: TextFormField(
           decoration: InputDecoration(
-              focusedBorder: OutlineInputBorder(
+              focusedBorder: const OutlineInputBorder(
                 borderSide: BorderSide(color: Color(0xFFe6e1e1), width: 1.0),
               ),
               enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Color(0xFFe6e1e1), width: 1.0),
+                  borderSide:
+                      const BorderSide(color: Color(0xFFe6e1e1), width: 1.0),
                   borderRadius: BorderRadius.circular(7)),
               fillColor: Colors.white,
               hintText: 'Add Your Promo Code',
               filled: true,
               suffixIcon: IconButton(
-                  icon: Icon(
+                  icon: const Icon(
                     Icons.local_offer,
                     color: Color(0xFFfd2c2c),
                   ),
@@ -302,6 +315,7 @@ class PromoCodeWidget extends StatelessWidget {
   }
 }
 
+// ignore: must_be_immutable
 class CartItem extends StatelessWidget {
   String productName;
   String productPrice;
@@ -309,11 +323,11 @@ class CartItem extends StatelessWidget {
   String productCartQuantity;
 
   CartItem({
-    Key key,
-    @required this.productName,
-    @required this.productPrice,
-    @required this.productImage,
-    @required this.productCartQuantity,
+    Key? key,
+    required this.productName,
+    required this.productPrice,
+    required this.productImage,
+    required this.productCartQuantity,
   }) : super(key: key);
 
   @override
@@ -321,9 +335,9 @@ class CartItem extends StatelessWidget {
     return Container(
       width: double.infinity,
       height: 130,
-      decoration: BoxDecoration(boxShadow: [
+      decoration: const BoxDecoration(boxShadow: [
         BoxShadow(
-          color: Color(0xFFfae3e2).withOpacity(0.3),
+          color: Color(0xFFfae3e2),
           spreadRadius: 1,
           blurRadius: 1,
           offset: Offset(0, 1),
@@ -332,33 +346,32 @@ class CartItem extends StatelessWidget {
       child: Card(
           color: Colors.white,
           elevation: 0,
-          shape: RoundedRectangleBorder(
-            borderRadius: const BorderRadius.all(
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(
               Radius.circular(5.0),
             ),
           ),
           child: Container(
             alignment: Alignment.center,
-            padding: EdgeInsets.only(left: 5, right: 5, top: 10, bottom: 10),
+            padding:
+                const EdgeInsets.only(left: 5, right: 5, top: 10, bottom: 10),
             child: Row(
               mainAxisSize: MainAxisSize.max,
               children: <Widget>[
-                Container(
-                  child: Align(
-                    alignment: Alignment.centerLeft,
-                    child: Center(
-                        child: Image.asset(
-                      "assets/images/popular_foods/$productImage.png",
-                      width: 110,
-                      height: 100,
-                    )),
-                  ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Center(
+                      child: Image.asset(
+                    "assets/images/popular_foods/$productImage.png",
+                    width: 110,
+                    height: 100,
+                  )),
                 ),
                 Column(
                   mainAxisSize: MainAxisSize.max,
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: <Widget>[
-                    SizedBox(
+                    const SizedBox(
                       height: 5,
                     ),
                     Row(
@@ -367,32 +380,28 @@ class CartItem extends StatelessWidget {
                         Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: <Widget>[
-                            Container(
-                              child: Text(
-                                "$productName",
-                                style: TextStyle(
-                                    fontSize: 18,
-                                    color: Color(0xFF3a3a3b),
-                                    fontWeight: FontWeight.w400),
-                                textAlign: TextAlign.left,
-                              ),
+                            Text(
+                              productName,
+                              style: const TextStyle(
+                                  fontSize: 18,
+                                  color: Color(0xFF3a3a3b),
+                                  fontWeight: FontWeight.w400),
+                              textAlign: TextAlign.left,
                             ),
-                            SizedBox(
+                            const SizedBox(
                               height: 5,
                             ),
-                            Container(
-                              child: Text(
-                                "$productPrice",
-                                style: TextStyle(
-                                    fontSize: 18,
-                                    color: Color(0xFF3a3a3b),
-                                    fontWeight: FontWeight.w400),
-                                textAlign: TextAlign.left,
-                              ),
+                            Text(
+                              productPrice,
+                              style: const TextStyle(
+                                  fontSize: 18,
+                                  color: Color(0xFF3a3a3b),
+                                  fontWeight: FontWeight.w400),
+                              textAlign: TextAlign.left,
                             ),
                           ],
                         ),
-                        SizedBox(
+                        const SizedBox(
                           width: 40,
                         ),
                         Container(
@@ -406,7 +415,7 @@ class CartItem extends StatelessWidget {
                       ],
                     ),
                     Container(
-                      margin: EdgeInsets.only(left: 20),
+                      margin: const EdgeInsets.only(left: 20),
                       alignment: Alignment.centerRight,
                       child: AddToCartMenu(2),
                     )
@@ -419,15 +428,18 @@ class CartItem extends StatelessWidget {
   }
 }
 
+// ignore: must_be_immutable
 class CartIconWithBadge extends StatelessWidget {
   int counter = 3;
+
+  CartIconWithBadge({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Stack(
       children: <Widget>[
         IconButton(
-            icon: Icon(
+            icon: const Icon(
               Icons.business_center,
               color: Color(0xFF3a3737),
             ),
@@ -437,18 +449,18 @@ class CartIconWithBadge extends StatelessWidget {
                 right: 11,
                 top: 11,
                 child: Container(
-                  padding: EdgeInsets.all(2),
+                  padding: const EdgeInsets.all(2),
                   decoration: BoxDecoration(
                     color: Colors.white,
                     borderRadius: BorderRadius.circular(6),
                   ),
-                  constraints: BoxConstraints(
+                  constraints: const BoxConstraints(
                     minWidth: 14,
                     minHeight: 14,
                   ),
                   child: Text(
                     '$counter',
-                    style: TextStyle(
+                    style: const TextStyle(
                       color: Colors.red,
                       fontSize: 8,
                     ),
@@ -462,52 +474,52 @@ class CartIconWithBadge extends StatelessWidget {
   }
 }
 
+// ignore: must_be_immutable
 class AddToCartMenu extends StatelessWidget {
   int productCounter;
 
-  AddToCartMenu(this.productCounter);
+  AddToCartMenu(this.productCounter, {Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.remove),
-            color: Colors.black,
-            iconSize: 18,
-          ),
-          InkWell(
-            onTap: () => print('hello'),
-            child: Container(
-              width: 100.0,
-              height: 35.0,
-              decoration: BoxDecoration(
-                color: Color(0xFFfd2c2c),
-                border: Border.all(color: Colors.white, width: 2.0),
-                borderRadius: BorderRadius.circular(5.0),
-              ),
-              child: Center(
-                child: Text(
-                  'Add To $productCounter',
-                  style: new TextStyle(
-                      fontSize: 12.0,
-                      color: Colors.white,
-                      fontWeight: FontWeight.w300),
-                ),
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.remove),
+          color: Colors.black,
+          iconSize: 18,
+        ),
+        InkWell(
+          // ignore: avoid_print
+          onTap: () => print('hello'),
+          child: Container(
+            width: 100.0,
+            height: 35.0,
+            decoration: BoxDecoration(
+              color: const Color(0xFFfd2c2c),
+              border: Border.all(color: Colors.white, width: 2.0),
+              borderRadius: BorderRadius.circular(5.0),
+            ),
+            child: Center(
+              child: Text(
+                'Add To $productCounter',
+                style: const TextStyle(
+                    fontSize: 12.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w300),
               ),
             ),
           ),
-          IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.add),
-            color: Color(0xFFfd2c2c),
-            iconSize: 18,
-          ),
-        ],
-      ),
+        ),
+        IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.add),
+          color: const Color(0xFFfd2c2c),
+          iconSize: 18,
+        ),
+      ],
     );
   }
 }
